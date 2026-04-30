7b092ead3349507bfdbe456fbd4c15851eb81577
junaidbarki <junaidbarki412824@gmail.com>
2026-04-30 10:27:42 +0500

    initial ktor hello world

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(ktorLibs.plugins.ktor)
+}
+
+group = "com.example"
+version = "1.0.0-SNAPSHOT"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+// ✅ Added for Railway fat JAR
+ktor {
+    fatJar {
+        archiveFileName.set("app.jar")
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+dependencies {
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.netty)
+    implementation(libs.logback.classic)
+
+    testImplementation(kotlin("test"))
+    testImplementation(ktorLibs.server.testHost)
+}
